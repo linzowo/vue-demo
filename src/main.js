e5e81ac66f7ff395960a9dc5929128327530c85a
linzowo <linzowo@outlook.com>
2019-08-27 15:09:00 +0800

    step-02-初始化顶部和底部导航栏

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 // 这是本项目的js入口文件
 
-console.log('hello world');
+// 引入vue
+import Vue from 'vue';
+
+
+// 按需引入mintui组件
+import { Header } from 'mint-ui';
+// 按需挂载mintui
+Vue.component(Header.name, Header);
+
+// 引入MUI样式库
+import './lib/mui/css/mui.css'
+
+// 引入项目主要app组件
+import app from './app.vue'
+
+// 创建vue实例
+var vm = new Vue({
+  el: '#app',
+  data: {},
+  methods: {},
+  render: c => c(app)
+})
+
 
