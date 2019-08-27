976d379def4369be8565c729603e17e4e4f22edb
linzowo <linzowo@outlook.com>
2019-08-27 14:21:36 +0800

    step01-初始化项目结构-安装必要配置

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// 这是本项目的js入口文件
+
+console.log('hello world');
+
---
+++ src/router.js
@@ -0,0 +1,13 @@
+// 路由模块
+
+// 引入vue-router
+import VueRouter from "vue-router";
+
+// 创建一个vue-router实例
+const router = new VueRouter({
+  routes: []
+});
+
+//   向外暴露router
+
+export default router;
