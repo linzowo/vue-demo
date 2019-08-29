49c23a3739419d0b450a2eadbfc6153d6fb009f9
linzowo <linzowo@outlook.com>
2019-08-29 12:00:37 +0800

    step-09-更改tabbar组件存放位置-新增新闻列表组件并建立路由

---
+++ src/components/news/NewsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>这是新闻列表</h1>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss" scope>
+    
+</style>
--- src/router.js
+++ src/router.js
@@ -4,21 +4,27 @@
 import VueRouter from "vue-router";
 
 // 引入路由组件
-import HomeContainer from './components/HomeContainer.vue'
-import MemberContainer from './components/MemberContainer.vue'
-import ShopcartContainer from './components/ShopcartContainer.vue'
-import SearchContainer from './components/SearchContainer.vue'
+
+// tabbar区域
+import HomeContainer from "./components/tabbar/HomeContainer.vue";
+import MemberContainer from "./components/tabbar/MemberContainer.vue";
+import ShopcartContainer from "./components/tabbar/ShopcartContainer.vue";
+import SearchContainer from "./components/tabbar/SearchContainer.vue";
+
+// 新闻列表区域
+import NewsList from "./components/news/NewsList.vue";
 
 // 创建一个vue-router实例
 const router = new VueRouter({
   routes: [
-    { path: '/', redirect: '/home'},
-    { path: '/home', component: HomeContainer},
-    { path: '/member', component: MemberContainer},
-    { path: '/shopcart', component: ShopcartContainer},
-    { path: '/search', component: SearchContainer},
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: HomeContainer },
+    { path: "/home/list", component: NewsList },
+    { path: "/member", component: MemberContainer },
+    { path: "/shopcart", component: ShopcartContainer },
+    { path: "/search", component: SearchContainer }
   ],
-  linkActiveClass: 'mui-active'
+  linkActiveClass: "mui-active"
 });
 
 //   向外暴露router
